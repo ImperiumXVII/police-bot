7aca98a4c948d183942a1fc00aef3f5cc16fd260
Michael (ImperiumXVII) <xviiimperium@gmail.com>
2024-05-16 11:52:34 +0100

    ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-    "type": "mysql",
-    "host": "imperiumxvii.co.uk",
-    "port": 3306,
-    "username": "lspd-bot",
-    "password": process.env.SQL_PASSWORD,
-    "database": "lspd",
-    // logging: true,
-    "entities": [__dirname + "/build/entities/*.js"]
+	type: 'mysql',
+	host: 'imperiumxvii.co.uk',
+	port: 3306,
+	username: 'lspd',
+	password: process.env.SQL_PASSWORD,
+	database: 'lspd',
+	// logging: true,
+	entities: [__dirname + '/build/entities/*.js'],
 };
